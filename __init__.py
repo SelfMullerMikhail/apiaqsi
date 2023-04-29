82457c36382743b4cb8cb314cedcc7fe3d096644
Preensikl <Preensikl@mail.ru>
2023-04-29 18:22:36 +0300

    29.04.2023

--- __init__.py
+++ __init__.py
@@ -9,3 +9,26 @@ from .aqsi_body.receipts import ReceiptsAqsi
 from .aqsi_body.shifts import ShiftsAqsi
 from .aqsi_body.shops import ShopsAqsi
 from .aqsi_body.slips import Slips
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(root_dir)
+
+from functions.aqsi_functions import AqsiFunctions
+from aqsi_types.aqsi_types import *
+
+API = ""
+aqsi = ApiAqsiClasses(API)
+
+shops = aqsi.get_shops()
+orders = aqsi.get_orders()
+goods = aqsi.get_goods()
+device = aqsi.get_devices()
+cachiers = aqsi.get_cashiers()
+reciepts = aqsi.get_receipts()
+slips = aqsi.get_slips()
+shifts = aqsi.get_shifts()
+goods_category = aqsi.get_goodsCategory()
+clients = aqsi.get_clietns()
+
+content_order = ContentOrder(type_= 4, discountMoney=5.5)
+orders.orders_create(id=2, number="4", content=content_order)
